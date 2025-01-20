61a214740c0a725462e707103c23c02b706e46d5
Ashish050488 <ashishar050488@gmail.com>
2025-01-20 21:48:30 +0530

    DesignCompleted

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import SideBar from '../../Components/sidebar/SideBar'
+import MessageContainer from '../../Components/messages/MessageContainer'
 
 const Home = () => {
   return (
     <div className='flex p-3  sm:h-[450px] md-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0 '>
       <SideBar/>
-      {/* <MessageContainer/> */}
+      <MessageContainer/>
     </div>
   )
 }
