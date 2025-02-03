c8e5f1141e7a7b72ff1ba460c040cb78c957fabf
Ashish050488 <ashishar050488@gmail.com>
2025-02-03 21:57:37 +0530

    SignUp_Completed

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import toast from 'react-hot-toast'
+import { useAuthContext } from "../context/AuthContext";
 
 const useSignup = () => {
   const [loading,setLoading] =useState(false);
+  const {setAuthUser} = useAuthContext()
+
 
   // const userData = { fullName, username, password, confirmPassword, gender }
 
@@ -24,6 +27,13 @@ const useSignup = () => {
         throw new Error(data.error)
        }
        console.log(data);
+
+       // Local storage
+       localStorage.setItem("chat-user",JSON.stringify(data))
+       // Context
+       setAuthUser(data)
+
+
        
     } catch (error) {
       toast.error(error.message)
