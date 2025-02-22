ed0055260d6bcc83c3bab03317f35a7fe270f04f
Ashish050488 <ashishar050488@gmail.com>
2025-02-22 13:02:12 +0530

    TRIED TO FIX CLICKING

--- frontend/src/Components/messages/MessageInput.jsx
+++ frontend/src/Components/messages/MessageInput.jsx
@@ -1,20 +1,27 @@
-import { useState } from 'react'; 
+import { useState,useRef } from 'react'; 
 import {BsSend} from 'react-icons/bs'
 import useSendMessage from '../../hooks/useSendMessage';
 
 const MessageInput = () => {
   const [message,setMessage] = useState('');
   const {loading,sendMessage}=useSendMessage();
+  const inputRef = useRef(null);
+
+
+
   const handleSubmit = async (e)=>{
     e.preventDefault();
     if(!message.trim()) return;
-    setMessage('');
     await sendMessage(message);
+    setMessage('');
+    inputRef.current && inputRef.current.focus();
   }
   return (
     <form className='px-4 my-3' onSubmit={handleSubmit}>
       <div className='w-full relative'>
         <input
+         ref={inputRef}
+         autoFocus
         type="text" 
         className='border text-sm rounded-lg block w-full p-2.5 bg-gray-700 border-gray-600 text-white' 
         placeholder='Send a message'
